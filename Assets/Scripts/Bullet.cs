d16f54733bdc475b5ca5beec98cee41daf8db66c
Rob <18713128+CaptainSloth@users.noreply.github.com>
2019-07-16 18:00:13 -0700

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void SeeknDestroy (Transform _target)
+    {
+        target = _target;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+	}
+
+    void HitTarget()
+    {
+        Debug.Log("BOOF");
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        Destroy(target.gameObject);
+
+        Destroy(gameObject);
+    }
+}
